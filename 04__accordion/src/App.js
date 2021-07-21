64921ccb3642ba0283342075c72cf680e4271544
NataliaSova <rot.fuchslein@gmail.com>
2021-07-21 17:27:31 +0500

    Accordion project

--- 04__accordion/src/App.js
+++ 04__accordion/src/App.js
@@ -2,7 +2,24 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return (
+    <main>
+      <section className='container'>
+        <h3>Questions And Answers About Login</h3>
+        <section className='info'>
+          {data.map((question) => {
+            return (
+              <SingleQuestion
+                key={question.id}
+                {...question}
+                r
+              ></SingleQuestion>
+            );
+          })}
+        </section>
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 04__accordion/src/Question.js
+++ 04__accordion/src/Question.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+
+const Question = ({ title, info }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setReadMore(!readMore)}>
+          {readMore ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      <p>{readMore ? info : `${info.substring(0, 0)}`}</p>
+    </article>
+  );
 };
 
 export default Question;

5124cd24310e0b50b00defa66a24e96fd0b9353f
NataliaSova <rot.fuchslein@gmail.com>
2021-07-23 19:12:35 +0500

    Slider project

--- 06__tabs/src/App.js
+++ 06__tabs/src/App.js
@@ -1,10 +1,69 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+import React, { useState, useEffect, useDebugValue } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+
+const url = 'https://course-api.com/react-tabs-project';
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className='section loadng'>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <span className='underline'></span>
+      </div>
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          {jobs.map((item, index) => {
+            return (
+              <button
+                key={item.id}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {item.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className='job-in'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-iceon' />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 }
 
-export default App
+export default App;
--- 07__slider/src/index.js
+++ 07__slider/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Alternative from './Alternative';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Alternative />
   </React.StrictMode>,
   document.getElementById('root')
 );

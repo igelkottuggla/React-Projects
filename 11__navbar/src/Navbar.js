9faf4ad1a1596e38614d6edb9cb3223d911c5baa
NataliaSova <rot.fuchslein@gmail.com>
2021-07-28 16:25:11 +0500

    Navbar project

--- 11__navbar/src/Navbar.js
+++ 11__navbar/src/Navbar.js
@@ -1,10 +1,65 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { icons } from 'react-icons/lib';
+import { links, social } from './data';
+import logo from './logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
 
-export default Navbar
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          <button
+            className='nav-toggle'
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+
+        <div
+          ref={linksContainerRef}
+          className={`${
+            showLinks ? 'links-container show-container ' : 'links-container'
+          } `}
+        >
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map((icon) => {
+            const { id, url, icon: img } = icon;
+            return (
+              <li key={id}>
+                <a href={url}>{img}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- 11__navbar/src/data.js
+++ 11__navbar/src/data.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
+import React from 'react';
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
 export const links = [
   {
     id: 1,
@@ -26,7 +26,7 @@ export const links = [
     url: '/profile',
     text: 'profile',
   },
-]
+];
 
 export const social = [
   {
@@ -49,4 +49,4 @@ export const social = [
     url: 'https://www.twitter.com',
     icon: <FaBehance />,
   },
-]
+];

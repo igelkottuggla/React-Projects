885081141356ed6803605c040e8f884a9c12a10f
NataliaSova <rot.fuchslein@gmail.com>
2021-08-09 17:01:30 +0500

    StockPhotos project

--- 19__stockPhotos/src/App.js
+++ 19__stockPhotos/src/App.js
@@ -1,12 +1,98 @@
-import React, { useState, useEffect } from 'react'
-import { FaSearch } from 'react-icons/fa'
-import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-const mainUrl = `https://api.unsplash.com/photos/`
-const searchUrl = `https://api.unsplash.com/search/photos/`
+import React, { useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [isLoading, setIsLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState('');
+
+  const fetchImages = async () => {
+    setIsLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`;
+
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`;
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) return data.results;
+        else if (query) return [...oldPhotos, ...data.results];
+        else return [...oldPhotos, ...data];
+      });
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+    // eslint-disable-next-line
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener('scroll', () => {
+      if (
+        !isLoading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((prevPage) => (prevPage === 0 ? 2 : prevPage + 1));
+      }
+    });
+    return () => window.removeEventListener('scroll', event);
+    // eslint-disable-next-line
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setPage(() => 1);
+    fetchImages();
+  };
+
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form'>
+          <label htmlFor='form-input'></label>
+          <input
+            type='text'
+            placeholder='Search...'
+            className='form-input'
+            value={query}
+            onChange={(event) => {
+              setQuery(event.target.value);
+            }}
+          />
+          <button type='submit' className='submit-btn' onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((photo) => {
+            return <Photo key={photo.id} {...photo} />;
+          })}
+        </div>
+        {isLoading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 19__stockPhotos/src/Photo.js
+++ 19__stockPhotos/src/Photo.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { FaHeart } from 'react-icons/fa';
 
-const Photo = () => {
-  return <h2>photo component</h2>
-}
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+}) => {
+  return (
+    <article className='photo'>
+      <img src={regular} alt={alt_description} />
+      <div className='photo-info'>
+        <div>
+          <h4>{name}</h4>
 
-export default Photo
+          <p className='likes-info'>
+            {likes} <FaHeart className='like' /> likes
+          </p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium} alt={name} className='user-img' />
+        </a>
+      </div>
+    </article>
+  );
+};
+
+export default Photo;

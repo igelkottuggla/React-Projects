e58d06f46cef9c9aea1e899288888c652caa34d8
NataliaSova <rot.fuchslein@gmail.com>
2021-08-10 12:05:43 +0500

    Dark mode project

--- 20__darkMode/src/App.js
+++ 20__darkMode/src/App.js
@@ -1,9 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import data from './data'
-import Article from './Article'
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import Article from './Article';
+
+const getStorageTheme = () => {
+  let theme = 'dark-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+};
 
 function App() {
-  return <h2>dark mode starter</h2>
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') setTheme('dark-theme');
+    else setTheme('light-theme');
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <main>
+      <div className='nav-center'>
+        <h1>Overracted</h1>
+        <button className='btn' onClick={toggleTheme}>
+          toggle theme
+        </button>
+      </div>
+      <section className='articles'>
+        {data.map((article) => {
+          return <Article key={article.id} {...article} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- 20__darkMode/src/Article.js
+++ 20__darkMode/src/Article.js
@@ -1,7 +1,17 @@
-import React from 'react'
-import moment from 'moment'
-const Article = () => {
-  return <h2>article component</h2>
-}
+import React from 'react';
+import moment from 'moment';
 
-export default Article
+const Article = ({ title, snippet, date, length }) => {
+  return (
+    <article className='post'>
+      <h2>{title}</h2>
+      <div className='post-info'>
+        <span>{moment(date).format('MMM dddd Do, YYYY, h:mm a')}</span>
+        <span>{length} min read</span>
+      </div>
+      <p>{snippet}</p>
+    </article>
+  );
+};
+
+export default Article;
